f4f43747b5ef34847c92d6623c31d4d5374ffcda
John Ingle <john.ingle@ihire.com>
2020-06-13 10:27:28 -0400

    Add Comments and run Autopep8

--- cityyouthmatrix/api/models.py
+++ cityyouthmatrix/api/models.py
@@ -2,26 +2,41 @@ from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
 from django.db import models
 
+
 class Driver(models.Model):
-    user_id = models.OneToOneField(User, primary_key=True, on_delete=models.CASCADE)
+    """Picks up or returns family and child(ren)
+    """
+    user_id = models.OneToOneField(
+        User, primary_key=True, on_delete=models.CASCADE)
     is_verified = models.BooleanField(default=False)
 
+
 class DriverNote(models.Model):
+    """Drivers must be able to:
+    Notate trips (Issues, concerns, positive experiences) 
+    """
     driver_id = models.ForeignKey(Driver, on_delete=models.CASCADE)
     note_text = models.CharField(max_length=500)
 
+
 class Trip(models.Model):
 
-    pickup_location = models.CharField(max_length=300) #todo - make this foreign key to a location entity?
+    # todo - make this foreign key to a location entity?
+    pickup_location = models.CharField(max_length=300)
     pickup_completed = models.BooleanField(default=False)
-    pickup_driver = models.ForeignKey(Driver, on_delete=models.DO_NOTHING, related_name="PickupDriver")
+    pickup_driver = models.ForeignKey(
+        Driver, on_delete=models.DO_NOTHING, related_name="PickupDriver")
 
-    return_location = models.CharField(max_length=300) #todo - make this foreign key to a location entity?
+    # todo - make this foreign key to a location entity?
+    return_location = models.CharField(max_length=300)
     return_complted = models.BooleanField(default=False)
-    return_driver = models.ForeignKey(Driver, on_delete=models.DO_NOTHING, related_name="ReturnDriver")
+    return_driver = models.ForeignKey(
+        Driver, on_delete=models.DO_NOTHING, related_name="ReturnDriver")
+
 
 class Family(models.Model):
     description = models.CharField(max_length=300, null=True)
+
 
 class FamilyMember(models.Model):
 
@@ -32,18 +47,35 @@ class FamilyMember(models.Model):
 
     user_type = models.CharField(
         max_length=1,
-        choices= FamilyMemberTypes.choices,
+        choices=FamilyMemberTypes.choices,
         default=FamilyMemberTypes.ADULT
     )
     name = models.CharField(max_length=500)
     family_id = models.ForeignKey(Family, on_delete=models.DO_NOTHING)
 
 
-
 class ActivityPartner(models.Model):
+    """
+    An organization hosting the extra-curricular event and learning experience. 
+    The location/destination information for a CYM-sponsored activity
+    The location stores the address for activity-partner or CYM location.
+    """
     name = models.CharField(max_length=100)
-    location = models.CharField(max_length=300) #todo - make this foreign key to a location entity?
+    # todo - make this foreign key to a location entity?
+    location = models.CharField(max_length=300)
+
 
 class Dispatcher(models.Model):
-    user_id = models.OneToOneField(User, primary_key=True, on_delete=models.CASCADE)
+    """coordinates pickup and return trips with family members, and drivers, and activity partner
+        This person should be considered an administrator unless a different actor turns up that needs more specialized permissions
+    """
+    user_id = models.OneToOneField(
+        User, primary_key=True, on_delete=models.CASCADE)
     contact_number = models.CharField(max_length=15)
+
+
+class Event(models.Model):
+    """Every event takes place at an activity partner's location
+    """
+    destination = models.ForeignKey(ActivityPartner, on_delete=models.CASCADE)
+    event_date = models.DateTimeField(auto_now=True)
