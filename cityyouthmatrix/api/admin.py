1b59ee74ef0d7eb922e20a7fcb77eade5a914dd7
Chad Hawkins <cwh@chadwhawkins.com>
2020-06-14 12:52:52 -0400

    Start tweaking models

--- cityyouthmatrix/api/admin.py
+++ cityyouthmatrix/api/admin.py
@@ -1,12 +1,29 @@
 from django.contrib import admin
-from .models import ActivityPartner, Dispatcher, Driver, DriverNote, Family, FamilyMember, Trip
-# Register your models here.
+from .models import (
+    ActivityPartner,
+    Driver,
+    Event,
+    Family,
+    FamilyAddress,
+    FamilyMember,
+    Trip,
+)
+
+
+# class BookInline(admin.TabularInline):
+#     model = Book
+
+# class AuthorAdmin(admin.ModelAdmin):
+#     inlines = [
+#         BookInline,
+#     ]
+
+
+
+
 
 admin.site.register(Driver)
-admin.site.register(DriverNote)
-
 admin.site.register(Trip)
 admin.site.register(Family)
 admin.site.register(FamilyMember)
 admin.site.register(ActivityPartner)
-admin.site.register(Dispatcher)
