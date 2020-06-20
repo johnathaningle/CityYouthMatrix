04f5d730d2c491a8fb9073028a348ffc8b2cbfe0
Veronica Furukawa <veronica.f@meetingplay.com>
2020-06-20 00:24:08 -0400

    html changes
    added more pages into html.
    virtualenv being wonky so I moved over the html to the templates>accounts
    tried to bring my main app.js to base.ts, but since I wasn't in virtual env. did not compile.

--- cityyouthmatrix/apps/accounts/static/accounts/scripts/base.ts
+++ cityyouthmatrix/apps/accounts/static/accounts/scripts/base.ts
@@ -8,13 +8,23 @@ export function initDashboard() {
         edge:'left'
     });
 
+     var elems = document.querySelectorAll('.modal');
+    var instance = M.Modal.init(elems,  {
+        opacity:.5
+    });
+
     var elems = document.querySelectorAll('.tabs');
     if(elems.length > 0) {
         var instance = M.Tabs.init(elems,  {
             swipeable:false
         });
     }
-
+    var elems = document.querySelectorAll('.select');
+    var instance = M.FormSelect.init(elems,  { });
+    var elems = document.querySelectorAll('.timepicker');
+    var instance = M.Timepicker.init(elems,  { });
+    var elems = document.querySelectorAll('.datepicker');
+    var instance = M.Datepicker.init(elems,  { });
     var elems = document.querySelectorAll('.dropdown-trigger');
     var instances = M.Dropdown.init(elems, {});
 }
