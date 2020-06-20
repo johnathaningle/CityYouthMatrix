c9cc372ec8be05115d0d1426ba2a19669b528b10
Veronica Furukawa <veronica.f@meetingplay.com>
2020-06-20 16:13:40 -0400

    committing these changed

--- cityyouthmatrix/apps/accounts/views.py
+++ cityyouthmatrix/apps/accounts/views.py
@@ -32,8 +32,41 @@ def broadcast(request: HttpRequest):
 def notifications(request: HttpRequest):
     return render(request, "accounts/dispatcher/notifications.html")  
 
+def driver_notifications(request: HttpRequest):
+    return render(request, "accounts/driver/notifications.html")      
+
 def dispatcher_profile(request: HttpRequest):
     return render(request, "accounts/dispatcher/profile.html")     
 
 def manage_rules(request: HttpRequest):
-    return render(request, "accounts/dispatcher/rules.html")        
+    return render(request, "accounts/dispatcher/rules.html")    
+
+def driver(request: HttpRequest):
+    return render(request, "accounts/driver/driver.html")   
+
+def family(request: HttpRequest):
+    return render(request, "accounts/family/family.html")      
+
+def driver_trip_info(request: HttpRequest):
+    return render(request, "accounts/driver/trip-info.html")     
+
+def driver_profile(request: HttpRequest):
+    return render(request, "accounts/driver/driver-profile.html")       
+
+def driver_site_rules(request: HttpRequest):
+    return render(request, "accounts/driver/site-rules.html")         
+
+def family_site_rules(request: HttpRequest):
+    return render(request, "accounts/family/site-rules.html")     
+
+def family_trip_info(request: HttpRequest):
+    return render(request, "accounts/family/trip-info.html")   
+
+def family_profile(request: HttpRequest):
+    return render(request, "accounts/family/family-profile.html")       
+
+def family_newtrip(request: HttpRequest):
+    return render(request, "accounts/family/new-trip.html")       
+
+def family_notifications(request: HttpRequest):
+    return render(request, "accounts/family/family-notifications.html")       
--- cityyouthmatrix/urls.py
+++ cityyouthmatrix/urls.py
@@ -21,6 +21,8 @@ from cityyouthmatrix.apps.api.views import (
 )
 from cityyouthmatrix.apps.accounts.views import (
     dispatcher,
+    driver,
+    family,
     home, 
     manage_drivers,
     manage_families,
@@ -31,7 +33,17 @@ from cityyouthmatrix.apps.accounts.views import (
     broadcast,
     notifications,
     dispatcher_profile,
-    manage_rules
+    manage_rules,
+    driver_trip_info,
+    driver_profile,
+    driver_site_rules,
+    family_site_rules,
+    family_trip_info,
+    family_profile,
+    driver_notifications,
+    family_newtrip,
+    family_notifications
+
 )
 
 urlpatterns = [
@@ -50,4 +62,15 @@ urlpatterns = [
     path("dispatcher/notifications", notifications),
     path("dispatcher/profile", dispatcher_profile),
     path("dispatcher/rules", manage_rules),
+    path("driver/driver", driver),
+    path("driver/tripinfo", driver_trip_info),
+    path("family/family", family),
+    path("driver/driverprofile", driver_profile),
+    path("family/familyprofile", family_profile),
+    path("driver/siterules", driver_site_rules),
+    path("family/siterules", family_site_rules),
+    path("family/tripinfo", family_trip_info),
+    path("driver/notifications", driver_notifications),
+    path("family/newtrip", family_newtrip),
+    path("family/familynotifications", family_notifications),
 ]
