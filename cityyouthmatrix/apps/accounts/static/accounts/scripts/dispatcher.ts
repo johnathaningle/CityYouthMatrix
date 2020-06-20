52bf861c120f33038b2149af386edb7a37b57fc1
cak <cak@fastmail.com>
2020-06-20 06:20:01 -0400

    Add google calendar support

--- cityyouthmatrix/apps/accounts/static/accounts/scripts/dispatcher.ts
+++ cityyouthmatrix/apps/accounts/static/accounts/scripts/dispatcher.ts
@@ -1,32 +1,40 @@
-
 /// <reference path="../typings/global.d.ts" />
 
+import { Calendar } from "@fullcalendar/core";
+import googleCalendarPlugin from "@fullcalendar/google-calendar";
+
 export function dispatch() {
+  if (
+    document.querySelectorAll(".datatable").length > 0 &&
+    document.querySelectorAll(".datatable2").length > 0
+  ) {
+    var dataTable = new DataTable(".datatable", {
+      perPage: 10,
+    });
+    var dataTable = new DataTable(".datatable2", {
+      perPage: 10,
+    });
+  }
 
-    if(document.querySelectorAll(".datatable").length > 0 && document.querySelectorAll(".datatable2").length > 0) {
-        var dataTable = new DataTable(".datatable",{
-            perPage:10
-        });
-        var dataTable = new DataTable(".datatable2",{
-            perPage:10
-        });
-    }
+  // var dataTable = new DataTable(".datatable3",{
+  // 	perPage:10
+  // });
+  // var dataTable = new DataTable(".datatable4",{
+  // 	perPage:10
+  // });
+  // init calendar
+  var calendarEl = document.getElementById("calendar");
 
-    // var dataTable = new DataTable(".datatable3",{
-    // 	perPage:10
-    // });
-    // var dataTable = new DataTable(".datatable4",{
-    // 	perPage:10
-    // });
-    // init calendar
-    var calendarEl = document.getElementById('calendar');
+  if (calendarEl != undefined) {
+    let calendar = new Calendar(calendarEl, {
+      plugins: [googleCalendarPlugin],
+      googleCalendarApiKey: "AIzaSyDzWcZv7q04t-8hbr2TkbKr-XFdKBJbMAs",
+      events: {
+        googleCalendarId:
+          "od9mode2m7q35qqi7u968pdfuk@group.calendar.google.com",
+      },
+    });
 
-    if(calendarEl != undefined) {
-        var calendar = new FullCalendar.Calendar(calendarEl, {
-            plugins: [ 'dayGrid' ]
-        });
-    
-        calendar.render();
-    }
+    calendar.render();
+  }
 }
-
