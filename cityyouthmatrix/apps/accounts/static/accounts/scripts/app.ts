7e8fe87c70d3849fbbbb6476946f851e9e57a71d
John Ingle <john.ingle@ihire.com>
2020-06-21 15:16:31 -0400

    Refactor Scripts and Add Jquery

--- cityyouthmatrix/apps/accounts/static/accounts/scripts/app.ts
+++ cityyouthmatrix/apps/accounts/static/accounts/scripts/app.ts
@@ -1,7 +1,23 @@
-import { dispatch } from "./dispatcher";
 import { initDashboard } from "./base";
+import  DispatcherController  from "./controllers/dispatcher/dispatcher";
+import DriverController from "./controllers/driver/driver";
 
 document.addEventListener("DOMContentLoaded", function() {
     initDashboard();
-    dispatch();
+    var app = new Application();
 });
+
+class Application {
+    constructor() {
+        let page = location.pathname;
+        let app: IController = null;
+        switch(page) {
+            case "/driver/driver":
+                app = new DriverController();
+        }
+        if (app == null) {
+            app = new DispatcherController();
+        }
+        app.handleLogic();
+    }
+}
---
+++ cityyouthmatrix/apps/accounts/static/accounts/scripts/controllers/driver/driver.ts
@@ -0,0 +1,9 @@
+
+import * as $ from "jquery";
+
+export default class DriverController implements IController {
+    handleLogic(): void {
+        console.log("driving!");
+    }
+
+}
